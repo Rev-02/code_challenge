0593b852581e7cdbdef63d4fd457f0d479baac19
Rev-02 <oliverward70@gmail.com>
2020-03-10 19:40:16 +0000

    fix input sanitisation

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 f = open("test_file.txt")
-lines = f.readlines()
-for line in lines:
-    line.strip()
-
+r_lines = f.readlines()
+lines = []
+for line in r_lines:
+    line = line.strip()
+    if len(line) != 0:
+        lines.append(line)
+print(lines)
 numCases = int(lines[0].strip())
 lines.pop(0)
 print(lines)
@@ -12,7 +15,7 @@ print(lines)
 for i in range(numCases):
     outputString = ""
     beautySum = 0
-    print(lines[i][0])
+    print(lines)
     for j in range(1,int(lines[i][0])):
         if (int(lines[i][j]) >= 0):
             beautySum += int(lines[i][j])
